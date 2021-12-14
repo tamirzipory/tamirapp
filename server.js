9732bb3c1c600ca0bdf2b4cd9239c214ff69c092
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2021-12-14 19:10:28 +0200

    update

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const express = require('express') ;
-const path = require("path");
-
-const app = express();
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'build')))
-app.get('/api', (req, res) =>{
-    res.json({
-        message: 'This is the api endpoint'
-    })
+
+//all your api code goes here
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'This is the api endpoint'
+  })
 })
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(5000, () =>
-console.log('server running'))
+app.listen(port, _ => {
+  console.log(`server started on port ${port}`)
+})
