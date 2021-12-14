06449904081816e38f97c715882c9e5ba1319cad
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2021-12-14 19:12:04 +0200

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000
 const path = require('path')
 
 app.use(express.static(path.join(__dirname, 'build')))
-
+app.get('/', (req, res) =>{res.send('hello')})
 //all your api code goes here
 app.get('/api', (req, res) => {
   res.json({
